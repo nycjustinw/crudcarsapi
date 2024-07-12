50ec85779067dc8f8e3a7f62193b1b9b53b98de3
nycjustinw <102558238+nycjustinw@users.noreply.github.com>
2024-07-11 23:57:59 -0400

    Update car.js

--- car.js
+++ car.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose');
 
-//validation
-
+//schematic for car that tracks make models and prices
 const carSchema = new mongoose.Schema({
   make: { type: String, required: true },
-  model: { type: String, required: true }
+  model: { type: String, required: true },
+  trim: {type: String, required: true},
+  color: {type: String, required: true},
+  cost: {type: Number, required: true}
 });
 
 const Car = mongoose.model('Car', carSchema);
